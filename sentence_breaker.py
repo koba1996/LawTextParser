ffb687ab0e1a9d7dc3003ebe64829a301a84a265
Balint Kozari <mrkoba@gmail.com>
2024-12-27 17:42:22 +0100

    raise minimum character match limit

--- sentence_breaker.py
+++ sentence_breaker.py
@@ -1,13 +1,28 @@
 import re
 
-test_file = open("test.txt", "r", encoding="utf8")
-text_to_break = test_file.read()
-text_to_break = re.sub(r"\(\d+([a-z]|[A-Z])?\)", "", text_to_break)
-text_to_break = re.sub(r"\*", "", text_to_break)
-text_to_break = re.sub(r"\n([a-z]|a[a-z])\)", "\n", text_to_break)
-text_to_break = re.sub(r"\[(.)*\]", "\n", text_to_break)
-text_to_break = re.sub(r"\d+(/[A-Z])*\.", "\n", text_to_break)
 
-print(text_to_break)
+def parse_exp_line(line):
+    number_range = re.split('\.? ', line)[1]
+    if '-' in number_range:
+        numbers = number_range.split('-')
+        return int(numbers[1]) - int(numbers[0]) + 1
+    else:
+        return 1
 
-sections = text_to_break.split(' § ')[1:]
+
+def prepare_sections(law, explanation):
+    law_file = open(law, "r", encoding="utf8")
+    law_text = law_file.read()
+    law_sections = re.split(r'\n\d+\. §.*\n', law_text)[1:]
+    exp_file = open(explanation, "r", encoding="utf8")
+    exp_text = exp_file.read()
+    exp_sections_raw = re.split(r'(\nA[z]? \d+.* §-hoz\n)', exp_text)[1:]
+    exp_sections = []
+    i = 0
+    while i < len(exp_sections_raw):
+        limit = parse_exp_line(exp_sections_raw[i])
+        i += 1
+        for _ in range(limit):
+            exp_sections.append(exp_sections_raw[i])
+        i += 1
+    return (law_sections, exp_sections)
